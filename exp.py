aa11ee1db81c614f268a9d335af2e08a4582a37d
Jonathan Long <jonlong@cs.berkeley.edu>
2011-08-27 18:54:55 -0700

    exp.py: Now storing metadata using dict.__repr__, keeping track of working dir

--- exp.py
+++ exp.py
@@ -21,6 +21,26 @@ import time
 RESULTS_PATH = 'results'
 DESCR_FILE = 'descr'
 
+# very simple serialization: dict.__repr__
+# human readable, no dependencies
+def save_descr(path, info):
+    """Save info about an experiment to a file
+
+    info is intended to be a dictionary of objects for which repr does the
+    right thing"""
+
+    f = open(path, 'w')
+    f.write(repr(info))
+    f.close()
+
+def load_descr(path):
+    """Load info about an experiment as saved by save_descr"""
+
+    f = open(path)
+    info = eval(f.read())
+    f.close()
+    return info
+
 def exec_cmd(args):
     p = subprocess.Popen(args)
     return os.waitpid(p.pid, 0)[1]
@@ -41,30 +61,19 @@ def trunc(s, n):
 def sha1(s):
     return hashlib.sha1(s).hexdigest()
 
-def abs_results_path():
-    # get the top level repo directory
-    topdir = exec_output(['git', 'rev-parse', '--show-toplevel']).strip()
-
-    return os.path.join(topdir, RESULTS_PATH)
-
-def read_descr(exp_dir):
-    f = open(os.path.join(abs_results_path(), exp_dir, DESCR_FILE), 'r')
-    hsh = f.readline().strip()
-    cmd = f.readline().strip()
-    date = f.readline().strip()
-    f.readline()
-    descr = f.readline().strip()
-    f.close()
-    return (hsh, cmd, date, descr)
+def abs_root_path():
+    return exec_output(['git', 'rev-parse', '--show-toplevel']).strip()
 
 def handle_existing(exp_dir):
-    if os.path.isdir(os.path.join(abs_results_path(), exp_dir)):
+    resultsdir = os.path.join(abs_root_path(), RESULTS_PATH, exp_dir)
+    if os.path.isdir(resultsdir):
         try:
-            date = read_descr(exp_dir)[2]
-        except:
+            date = load_descr(os.path.join(resultsdir, DESCR_FILE))['date']
+        except Exception as e:
             print ('A results directory already exists for this experiment, but\n'
                    ' the description cannot be read. This indicates a problem with\n'
                    ' this script that must be fixed to continue.')
+            print e
             return True
 
         print ('This experiment (' + trunc(exp_dir, 6) + ') appears'
@@ -85,7 +94,8 @@ def run_exp(branch, cmd, descr):
     # compute a hash unique to this experiment
     exp_hsh = sha1(hsh + cmd)
 
-    resultsdir = abs_results_path()
+    rootdir = abs_root_path()
+    resultsdir = os.path.join(rootdir, RESULTS_PATH)
 
     # create the results directory if necessary
     if not os.path.isdir(resultsdir):
@@ -100,13 +110,10 @@ def run_exp(branch, cmd, descr):
     os.mkdir(exp_path)
 
     # save the experimental description
-    f = open(os.path.join(exp_path, DESCR_FILE), 'w')
-    f.write(hsh + '\n')
-    f.write(cmd + '\n')
-    f.write(time.ctime() + '\n')
-    f.write('\n')
-    f.write(descr)
-    f.close()
+    info = {'commit': hsh, 'command': cmd, 'date': time.ctime(),
+            'description': descr,
+            'working_dir': os.path.relpath(os.getcwd(), rootdir)}
+    save_descr(os.path.join(exp_path, DESCR_FILE), info)
 
     # run the experiment
     new_cmd = cmd.replace('{}', exp_path)
@@ -115,23 +122,26 @@ def run_exp(branch, cmd, descr):
     print 'Command exited with status ' + str(sts)
     
 def read_descrs():
+    resultsdir = os.path.join(abs_root_path(), RESULTS_PATH)
     exps = {}
-    exp_dirs = os.listdir(abs_results_path())
+    exp_dirs = os.listdir(resultsdir)
     for exp_dir in exp_dirs:
         try:
-            exps[exp_dir] = read_descr(exp_dir)
-        except:
+            exps[exp_dir] = load_descr(os.path.join(resultsdir,
+                                                    exp_dir, DESCR_FILE))
+        except Exception as e:
             print 'Error reading description for ' + exp_dir
+            print e
             sys.exit(1)
     return exps
 
 def list_descrs(exps):
-    for (exp_hsh, (hsh, cmd, date, descr)) in exps.iteritems():
+    for (exp_hsh, info) in exps.iteritems():
         print (trunc(exp_hsh, 6) + '\t'
-               + trunc(hsh, 6) + '\t'
-               + trunc(cmd, 20) + '\t'
-               + date + '\t'
-               + trunc(descr, 30))
+               + trunc(info['commit'], 6) + '\t'
+               + trunc(info['command'], 20) + '\t'
+               + info['date'] + '\t'
+               + trunc(info['description'], 30))
 
 
 if __name__ == '__main__':
