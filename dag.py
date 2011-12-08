f25d2fed97a5e5a67b7b14e7d56b1133473974ef
David Moore <davmre@gmail.com>
2011-12-08 10:42:24 -0800

    add dag class for methods that touch the whole dag

--- dag.py
+++ dag.py
@@ -1,26 +1,94 @@
 
+# TODO: distinguish different failure modes
+[RUN_STATE_VIRGIN, RUN_STATE_RUNNING, RUN_STATE_SUCCESS, RUN_STATE_FAIL] = range(4) 
+
+
+class dag:
+
+    def __init__(self, toplevel_nodes):
+
+        # sort nodes topologically into dag_nodes
+        self.dag_nodes_reversed = []
+        for n in toplevel_nodes:
+            self.visit(n)
+        self.dag_nodes = [x for x in reversed(self.dag_nodes_reversed)]
+        for n in dag_nodes:
+            n.visited = False
+
+    # helper method for topological sort
+    def visit(self, node):
+        if node.visited == False:
+            node.visited=True
+            for m in node.children:
+                self.visit(m)
+            self.dag_nodes_reversed.append(node)
+
+    def update_states(self, black_box):
+        for node in self.dag_nodes:
+            if node.run_state == NODE_STATE_RUNNING:
+                node.run_state = black_box.getState(node)
+                
+
+    def run_runnable_jobs(self, black_box):
+        for node in self.dag_nodes:
+            if node.is_runnable():
+                if node.code is not None:
+                    try:
+                        eval(node.code)
+                        node.run_state = RUN_STATE_SUCCESS
+                    except:
+                        node.run_state = RUN_STATE_FAIL
+                else:
+                    self.jobid = black_box.run(node)
+                    node.run_state = RUN_STATE_RUNNING
 
 class dag_node:
      
-
-    def __init__(self, desc, command, params, commit, parents = None, children = None):
+    def __init__(self, desc, params, commit, command = None, code = None, parents = None, children = None):
         
         self.desc = desc # description (string)
+
+        # exactly one of these should be set
+        if command is not None and code is not None:
+            raise Exception("command and code are mutually exclusive")
         self.command = command # command to run (string)
+        self.code = code # code to execute
+
         self.params = params # parameters to pass (dictionary)
         self.commit = commit # commit hash (string)
-    
+
+
+        self.parents = set()
+        self.children = set()
         self.parents += parents
         self.children += children
+
+        self.run_state = self.readStateFromDisk() 
+        self.jobid = None
+
+        self.visited = False
 
     def add_parents(self, parents):
         self.parents += parents
+        for parent in parents:
+            parent.children += self
 
     def add_children(self, children):
         self.children += children
+        for child in children:
+            child.parents += self
+
+    def is_runnable(self):
+        parents_succeeded = all([p.run_state == RUN_STATE_SUCCESS for p in self.parents])
+        return run_state == RUN_STATE_VIRGIN and parents_succeded
+            
+
+    def update_node_states(self, )
+
+while not done running everything:
+    poll and update run state at each node
+    walk over nodes, and run each node if possible
 
 
-        
 
-
-
+    
