f90c8f989964907c2903c94589a11dd353e218d6
bharath2 <bharath2@ubuntu.ubuntu-domain>
2011-12-11 23:04:13 -0800

    reduce all params

--- special_macros.py
+++ special_macros.py
@@ -1,7 +1,7 @@
 import dag
 import os
 #List of macros
-macro_list=['produce_output_list_macro', 'produce_annotated_list_macro', 'produce_parameter_map_macro']
+macro_list=['produce_output_list_macro', 'produce_annotated_list_macro', 'produce_parameter_map_macro', 'produce_all_map_macro']
 
 # Creates a file containing all output directories.
 def produce_output_list_macro(node):
@@ -41,6 +41,41 @@ def produce_parameter_map_macro(param_name, node):
             f.write('\n')
         f.close()
         print "done."
+
+
+def produce_all_map_macro(outfile, node):
+    output_path=os.path.join(node.exp_results, 'param_out')
+    header=False
+    with open(output_path, 'w') as f:
+        print "writing parameter map to file %s ..." % (output_path),
+        for x in node.parents:
+            try:
+                fi = open(os.path.join(x.exp_results, outfile), 'r')
+            except IOError:
+                print "Error: could not open output file '%s' from job '%s'" % (os.path.join(x.exp_results, 'out'), x.info['description'])
+                exit(1)
+            if not header:
+                f.write('# ')
+                for param_key in x.params:
+                    f.write(param_key+' ')
+                f.write('output_val\n')
+                header=True
+                
+            for param_key in x.params:
+                val=x.params[param_key]
+                if(isinstance(val,type('string'))):
+                    print "here2"
+                    f.write('"'+val+'" ')
+                else:
+                    f.write(str(val)+' ')
+            param_val = list(fi)[0].strip()
+            fi.close()
+            f.write(param_val)
+            f.write('\n')
+        f.close()
+        print "done."
+
+
 
 def check_for_macro(macro_str):
     parts=macro_str.partition('(')
